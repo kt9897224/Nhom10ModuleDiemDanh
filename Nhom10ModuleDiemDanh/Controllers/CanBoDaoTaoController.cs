feb47b42bff47df41ecdbdb8e9332c43c0f2044e
ngovanhuyhoang <ngovanhuyhoang424@gmail.com>
2025-05-27 15:06:16 +0700

    hhbkhkhk

---
+++ Nhom10ModuleDiemDanh/Controllers/CanBoDaoTaoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Nhom10ModuleDiemDanh.Controllers
+{
+    public class CanBoDaoTaoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("CanBoDaoTao");
+        }
+    }
+}
